5b87b0d758c4e3b63d996fda87194483a83c14cf
AudreyLovelace <yueyueaudrey2021@gmail.com>
2022-01-12 12:20:43 -0400

    start up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <a href="https://www.shecodes.io/" title="shecodes">
+        <img
+          alt="shecodes"
+          src="https://www.shecodes.io/assets/branding/logo-shecodes-f9fa0540d113c086f61eb6e89466c0cbd24a42163b6a96d4b01da078803f53ee.png"
+        />
+      </a>
+      <div className="part">
+        <h2>What word do you want to look up?</h2>
+      </div>
+    </div>
+  );
 }
 
 export default App;
